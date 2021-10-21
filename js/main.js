f2d8831a552b27c5f5969704bf16f105bbaf63ab
Fernando Aguiar <89427878+Farp89@users.noreply.github.com>
2021-10-21 20:56:14 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+			const linksSocialMedia = {
+				github : 'Farp89',
+				youtube : 'channel/UCXxFo0BrL1846P5IaLNlgcA',
+				facebook : 'fernando.aguiar.16100',
+				instagram : 'tsael_89'
+			} 
+			function changeSocialMediaLinks(){
+					for (let li of socialLinks.children){
+						const social = li.getAttribute('class')
+						li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+					}
+			}
+			changeSocialMediaLinks()
+
+			function getGitHubProfileInfos(){
+				const url = `https://api.github.com/users/${linksSocialMedia.github}`
+				fetch(url)
+				.then(response=>response.json())
+				.then(data => {
+					userName.textContent = data.name
+					userBio.textContent = data.bio
+					userLink.src = data.avatar_url
+					userLink.href = data.html_url
+					userLogin.textContent = data.login
+				})
+			}
+			getGitHubProfileInfos()
